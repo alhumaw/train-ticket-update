099d5ab535fa5385d7f463cc1be3622feb498fbc
alhumaw <alhumaw47@gmail.com>
2025-02-15 19:14:19 -0800

    firstclass-30?

--- ts-order-service/src/main/java/order/init/InitData.java
+++ ts-order-service/src/main/java/order/init/InitData.java
@@ -20,16 +20,16 @@ public class InitData implements CommandLineRunner {
     String accountId = "4d2a46c7-71cb-4cf1-b5bb-b68406d9da6f";
     String contactName = "Contacts_One";
     String contactDocumentNumber = "DocumentNumber_One";
-    String firstClass = "FirstClass-30";
+    String firstClass = "30";
     String price = "100.0";
 
     @Override
-    public void run(String... args)throws Exception{
+    public void run(String... args) throws Exception {
         Order order = new Order();
         order.setId(UUID.fromString("5ad7750b-a68b-49c0-a8c0-32776b067703"));
         order.setBoughtDate(new Date());
-        order.setTravelDate(new Date("Sat Jul 29 00:00:00 GMT+0800 2017")); //NOSONAR
-        order.setTravelTime(new Date("Mon May 04 09:02:00 GMT+0800 2013")); //NOSONAR
+        order.setTravelDate(new Date("Sat Jul 29 00:00:00 GMT+0800 2017")); // NOSONAR
+        order.setTravelTime(new Date("Mon May 04 09:02:00 GMT+0800 2013")); // NOSONAR
         order.setAccountId(UUID.fromString(accountId));
         order.setContactsName(contactName);
         order.setDocumentType(1);
@@ -44,12 +44,11 @@ public class InitData implements CommandLineRunner {
         order.setPrice(price);
         service.initOrder(order, null);
 
-
         Order orderTwo = new Order();
         orderTwo.setId(UUID.fromString("8177ac5a-61ac-42f4-83f4-bd7b394d0531"));
         orderTwo.setBoughtDate(new Date());
-        orderTwo.setTravelDate(new Date("Sat Jul 29 00:00:00 GMT+0800 2017")); //NOSONAR
-        orderTwo.setTravelTime(new Date("Mon May 04 09:01:00 GMT+0800 2013")); //NOSONAR
+        orderTwo.setTravelDate(new Date("Sat Jul 29 00:00:00 GMT+0800 2017")); // NOSONAR
+        orderTwo.setTravelTime(new Date("Mon May 04 09:01:00 GMT+0800 2013")); // NOSONAR
         orderTwo.setAccountId(UUID.fromString(accountId));
         orderTwo.setContactsName(contactName);
         orderTwo.setDocumentType(1);
@@ -67,8 +66,8 @@ public class InitData implements CommandLineRunner {
         Order orderThree = new Order();
         orderThree.setId(UUID.fromString("d3c91694-d5b8-424c-9974-e14c89226e49"));
         orderThree.setBoughtDate(new Date());
-        orderThree.setTravelDate(new Date("Sat Jul 29 00:00:00 GMT+0800 2017")); //NOSONAR
-        orderThree.setTravelTime(new Date("Mon May 04 09:00:00 GMT+0800 2013")); //NOSONAR
+        orderThree.setTravelDate(new Date("Sat Jul 29 00:00:00 GMT+0800 2017")); // NOSONAR
+        orderThree.setTravelTime(new Date("Mon May 04 09:00:00 GMT+0800 2013")); // NOSONAR
         orderThree.setAccountId(UUID.fromString(accountId));
         orderThree.setContactsName(contactName);
         orderThree.setDocumentType(1);
